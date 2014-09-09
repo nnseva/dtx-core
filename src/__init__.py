50e84c40685af7a3082e4959c67c5cbfc840782e
TigerND <tigernwh@gmail.com>
2014-09-09 19:19:40 +0400

    Version related fix

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,7 @@
 
 import pkg_resources
 try:
-    version=pkg_resources.get_distribution('dtx').version
+    version=pkg_resources.get_distribution('dtx-core').version
 except:
     version="0.0.0"
 revision=int('$Id: __init__.py 15888 2013-10-11 04:53:59Z azykov $'.split(' ')[2])
