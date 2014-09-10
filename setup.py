e5ef2abf1d1d8e9c3427979e5bc8241918b121f0
TigerND <tigernwh@gmail.com>
2014-09-10 12:59:09 +0400

    Version has been updated

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='dtx-core',
-    version='0.9.6',
+    version='0.9.7',
     description='Django Twisted Extensions - Core',
     author='Alexander Zykov',
     author_email='tigernwh@gmail.com',
