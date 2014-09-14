bd21e982e05d88274056c9055fab6ab25caf42e4
TigerND <tigernwh@gmail.com>
2014-09-14 04:42:37 +0400

    Just some additional debug info

--- src/telnet/server/node.py
+++ src/telnet/server/node.py
@@ -12,5 +12,6 @@ log = logger.log(__name__)
 def start(host, port, colored=True):
     with log.enter() as tm:
         for family, socktype, proto, canonname, sockaddr in socket.getaddrinfo(host, port, 0, 0, socket.SOL_TCP):
+            log.msg(u'AddrInfo: {}'.format((family, socktype, proto, canonname, sockaddr)))
             sa_host, sa_port = sockaddr
             log.err('NOT IMPLEMENTED')
