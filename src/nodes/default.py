787188ac77d2a263af5bb7ea46c6802bd3d9056f
Aleksandr Zykov <alex.zykov@plexrayinc.com>
2016-05-02 16:33:52 +0200

    Updated

--- src/nodes/default.py
+++ src/nodes/default.py
@@ -14,16 +14,17 @@ def config(**kwargs):
         'DTX_WEB_HOST': [
             u'127.0.0.1',
         ],
-        'DTX_WEB_PORT': 80 if settings.DEBUG else 8000
+        'DTX_WEB_PORT': 80 if settings.DEBUG else 8000,
         'DTX_WEB_SITES': [
             (
                 u'.*',
                 'wsgi',
             ),
-        ]
+        ],
     }
-    port = kwargs.get('port', settings.DTX_WEB_PORT)
-    result.port = port if port
+    port = kwargs.get('port', settings.DTX_WEB_PORT if hasattr(settings, 'DTX_WEB_PORT') else 8000)
+    if (port):
+        result['DTX_WEB_PORT'] = port
     return result
 
 def start(**kwargs):
@@ -33,14 +34,16 @@ def start(**kwargs):
     
     try:
         from setproctitle import setproctitle
-        setproctitle('django-twisted @ port {}'.format(conf.DTX_WEB_HOST))
+        setproctitle('django-twisted @ port {}'.format(conf['DTX_WEB_HOST']))
     except:
         pass
 
-    for host in settings.DTX_PUBLIC_ADDRESSES:
-        log.msg('Starting web node at {}:{}...'.format(conf.DTX_WEB_HOST, conf.DTX_WEB_PORT))
+    port = conf['DTX_WEB_PORT']
+    sites = conf['DTX_WEB_SITES']
+    for host in conf['DTX_WEB_HOST']:
+        log.msg('Starting web node at {}:{}...'.format(host, port))
         node.start('dtx.web.server.node',
-            host=conf.DTX_WEB_HOST, 
-            port=conf.DTX_WEB_PORT,
-            sites=conf.DTX_WEB_SITES
+            host=host, 
+            port=port,
+            sites=sites
         )
